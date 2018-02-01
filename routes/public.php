da3229dba70b87be75ab6701d03a730e3a67d280
JhonatanCF <chiquifu55@gmail.com>
2018-01-31 21:47:46 -0500

    Lesson #12 Show post details with TDD

--- routes/public.php
+++ routes/public.php
@@ -19,4 +19,9 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index');
 
+Route::get('posts/{post}', [
+	'as' 	=> 'posts.show',
+	'uses' 	=> 'PostController@show'
+])->where('post', '\d+'); //[0-9]+
+
 
