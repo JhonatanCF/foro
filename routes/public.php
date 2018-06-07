ae5020aa7d7069fe9a53d7b5770752edf6e4cda0
JhonatanCF <chiquifu55@gmail.com>
2018-06-06 20:52:49 -0500

    List posts: Allow posts to be filtered by status AND category at the same time

--- routes/public.php
+++ routes/public.php
@@ -19,12 +19,12 @@ Route::get('posts/{post}-{slug}', [
 	'uses' 	=> 'PostController@show'
 ])->where('post', '\d+'); //[0-9]+
 
-Route::get('posts-pendientes', [
+Route::get('posts-pendientes/{category?}', [
     'uses' => 'PostController@index',
     'as' => 'posts.pending'
 ]);
 
-Route::get('posts-completados', [
+Route::get('posts-completados/{category?}', [
     'uses' => 'PostController@index',
     'as' => 'posts.completed'
 ]);
