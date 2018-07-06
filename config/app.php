4a3bd83aeacd384ecd5818964f9d0a6074609c2a
JhonatanCF <chiquifu55@gmail.com>
2018-07-05 22:43:01 -0500

    Lesson 46 Solution to problem N+1

--- config/app.php
+++ config/app.php
@@ -169,6 +169,7 @@ return [
         Styde\Html\HtmlServiceProvider::class,
         GrahamCampbell\Markdown\MarkdownServiceProvider::class,
         \Laravel\Tinker\TinkerServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
 
         //
 
@@ -230,7 +231,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Access' => Styde\Html\Facades\Access::class,
         'Markdown' => GrahamCampbell\Markdown\Facades\Markdown::class,
-
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
     ],
 
 ];
