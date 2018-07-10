75756b757f4c6ce0a13f180bf6283e8101307909
JhonatanCF <chiquifu55@gmail.com>
2018-07-09 19:21:51 -0500

    Lesson #51 API to vote for a Post, part 1

---
+++ app/Http/Controllers/VotePostController.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\{Post, Vote};
+use Illuminate\Http\Request;
+
+class VotePostController extends Controller
+{
+    public function upvote(Post $post)
+    {
+        Vote::upvote($post);
+        return [
+            'new_score' => $post->score,
+        ];
+    }
+}
